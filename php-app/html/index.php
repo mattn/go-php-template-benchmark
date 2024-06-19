8b9c08921ca8ea12544f6f8b7dc53270b757c9a2
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-06-19 13:08:42 +0900

    use php-fpm

--- php-app/html/index.php
+++ php-app/html/index.php
@@ -3,7 +3,8 @@
 <ul>
 <?php
 require_once('/app/vendor/smarty/smarty/libs/Smarty.class.php');
-$smarty = new Smarty();
+$smarty = new Smarty\Smarty();
+$smarty->setCompileDir('/tmp');
 $json = file_get_contents("input.json");
 $data = json_decode($json, true);
 $smarty->assign('data', $data, true);
